cd064a3dfa6b72645f9e8662556946581133635a
priyadarsinisahu <priyadarsinisahu27@gmail.com>
2026-08-26 12:32:29 +0530

    File exists

--- var/scripts/check_file.sh
+++ var/scripts/check_file.sh
@@ -1,3 +1,7 @@
+<<comment 
+Problem: Create a script that accepts a filename as an argument and checks whether the file exists. 
+comment 
+
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
@@ -14,3 +18,9 @@ else
     echo "Error: File '$FILE' does not exist."
     exit 1
 fi
+
+<<comment
+User input: ./check_file.sh practice_order.txt
+Expected output: File 'practice_order.txt' exists.
+Error output: Error: File 'practice_order.txt' does not exist.
+comment
