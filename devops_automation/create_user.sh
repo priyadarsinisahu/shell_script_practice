9586d08c2343f5e855657c1f3f8db302aa2a32bd
priyadarsinisahu <priyadarsinisahu27@gmail.com>
2026-08-27 18:46:27 +0530

    create users

---
+++ devops_automation/create_user.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+<<info
+This is a shell script to create users
+info
+
+
+echo "==========  Creation of User Started =========="
+
+read -p "enter the username:" username
+
+read -p "enter the password:" password
+
+
+#user delete
+sudo useradd -m "$username"
+
+echo -e "$password\n$password" | sudo passwd "$username" 
+
+echo "==========  Creation of User Completed =========="
+
+sudo userdel $username
+
+echo "==========  Deletion of User Completed =========="
+
+# user check
+
+if [ $(cat /etc/passwd | grep $username | wc | awk '{print $1}') == 0 ];
+then
+
+	echo "as wc is 0 the user is deleted"
+else
+	echo "the user was not deleted"
+fi
